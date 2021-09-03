231bc4346ec45ef9663223252cbd5b61a79529e3
nikola4468 <nikola4468@gmail.com>
2021-09-03 10:50:44 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int refill = 1090;
+        int bonus = 0;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+            balance = balance + refill + bonus;
+        } else {
+            balance = balance + refill;
+        }
+        System.out.println(bonus);
+        System.out.println(balance);
+
+    }
+}
